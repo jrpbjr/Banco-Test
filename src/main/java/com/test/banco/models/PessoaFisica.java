ac05b7ca20bfaa718737d9a797bc6817090c6994
jrpbjr <jrpbjr@gmail.com>
2024-01-15 00:41:06 -0300

    refatoracao, remover no mãe

--- src/main/java/com/test/banco/models/PessoaFisica.java
+++ src/main/java/com/test/banco/models/PessoaFisica.java
@@ -23,20 +23,16 @@ public class PessoaFisica extends Pessoa{
     @Column(length = 20, nullable = false)
     private LocalDate dt_nascimento;
 
-    @Column(nullable = false)
-    private String nome_mae;
-
     @Column(length = 20, nullable = false)
     @Enumerated(EnumType.STRING)
     private Genero genero;
 
 
-    public PessoaFisica(Long id, String nome, String email, String telefone, String celular, Endereco endereco, Conta conta, LocalDateTime criado_em, LocalDateTime alterado_em, boolean status, String cpf, String rg, LocalDate dt_nascimento, String nome_mae, Genero genero) {
+    public PessoaFisica(Long id, String nome, String email, String telefone, String celular, Endereco endereco, Conta conta, LocalDateTime criado_em, LocalDateTime alterado_em, boolean status, String cpf, String rg, LocalDate dt_nascimento,  Genero genero) {
         super(id, nome, email, telefone, celular, endereco, conta, criado_em, alterado_em, status);
         this.cpf = cpf;
         this.rg = rg;
         this.dt_nascimento = dt_nascimento;
-        this.nome_mae = nome_mae;
         this.genero = genero;
     }
 
