8c2a9ef7545e7318e7653c0cada24dee9dd8d0d1
jrpbjr <jrpbjr@gmail.com>
2024-01-17 22:35:08 -0300

    Objeto para receber o valor da mensagem true ou false.

---
+++ src/main/java/com/test/banco/models/Messagem.java
@@ -0,0 +1,13 @@
+package com.test.banco.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@NoArgsConstructor
+public class Messagem {
+    @Getter
+    @Setter
+    private Boolean messageSent;
+}
